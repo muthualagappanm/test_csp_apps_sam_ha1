59a5b39a9e8be6b7984fa6fd49e62775320788ed
Amit Raddi <amit.raddi@microchip.com>
2020-08-12 14:20:35 +0530

    [SERCOM] Regenerated demo for sercom_spi_spi_eeprom_write_read_sam_ha1_xpro
    MH3-43556
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/sercom/spi/spi_eeprom_write_read/firmware/src/config/sam_ha1_xpro/peripheral/port/plib_port.h
+++ apps/sercom/spi/spi_eeprom_write_read/firmware/src/config/sam_ha1_xpro/peripheral/port/plib_port.h
@@ -65,36 +65,32 @@
 // *****************************************************************************
 // *****************************************************************************
 
-  
 /*** Macros for LED pin ***/
 #define LED_Set()               (PORT_REGS->GROUP[0].PORT_OUTSET = 1 << 0)
 #define LED_Clear()             (PORT_REGS->GROUP[0].PORT_OUTCLR = 1 << 0)
 #define LED_Toggle()            (PORT_REGS->GROUP[0].PORT_OUTTGL = 1 << 0)
-#define LED_Get()               (((PORT_REGS->GROUP[0].PORT_IN >> 0)) & 0x01)
 #define LED_OutputEnable()      (PORT_REGS->GROUP[0].PORT_DIRSET = 1 << 0)
 #define LED_InputEnable()       (PORT_REGS->GROUP[0].PORT_DIRCLR = 1 << 0)
+#define LED_Get()               (((PORT_REGS->GROUP[0].PORT_IN >> 0)) & 0x01)
 #define LED_PIN                  PORT_PIN_PA00
 
 /*** Macros for EEPROM_WP pin ***/
 #define EEPROM_WP_Set()               (PORT_REGS->GROUP[1].PORT_OUTSET = 1 << 6)
 #define EEPROM_WP_Clear()             (PORT_REGS->GROUP[1].PORT_OUTCLR = 1 << 6)
 #define EEPROM_WP_Toggle()            (PORT_REGS->GROUP[1].PORT_OUTTGL = 1 << 6)
-#define EEPROM_WP_Get()               (((PORT_REGS->GROUP[1].PORT_IN >> 6)) & 0x01)
 #define EEPROM_WP_OutputEnable()      (PORT_REGS->GROUP[1].PORT_DIRSET = 1 << 6)
 #define EEPROM_WP_InputEnable()       (PORT_REGS->GROUP[1].PORT_DIRCLR = 1 << 6)
+#define EEPROM_WP_Get()               (((PORT_REGS->GROUP[1].PORT_IN >> 6)) & 0x01)
 #define EEPROM_WP_PIN                  PORT_PIN_PB06
 
 /*** Macros for EEPROM_CS pin ***/
 #define EEPROM_CS_Set()               (PORT_REGS->GROUP[0].PORT_OUTSET = 1 << 10)
 #define EEPROM_CS_Clear()             (PORT_REGS->GROUP[0].PORT_OUTCLR = 1 << 10)
 #define EEPROM_CS_Toggle()            (PORT_REGS->GROUP[0].PORT_OUTTGL = 1 << 10)
-#define EEPROM_CS_Get()               (((PORT_REGS->GROUP[0].PORT_IN >> 10)) & 0x01)
 #define EEPROM_CS_OutputEnable()      (PORT_REGS->GROUP[0].PORT_DIRSET = 1 << 10)
 #define EEPROM_CS_InputEnable()       (PORT_REGS->GROUP[0].PORT_DIRCLR = 1 << 10)
+#define EEPROM_CS_Get()               (((PORT_REGS->GROUP[0].PORT_IN >> 10)) & 0x01)
 #define EEPROM_CS_PIN                  PORT_PIN_PA10
-
-
-
 // *****************************************************************************
 /* PORT Group
 
@@ -238,6 +234,15 @@ typedef enum
 
     /* PB17 pin */
     PORT_PIN_PB17 = 49,
+
+    /* PB22 pin */
+    PORT_PIN_PB22 = 54,
+
+    /* PB23 pin */
+    PORT_PIN_PB23 = 55,
+
+    /* PB30 pin */
+    PORT_PIN_PB30 = 62,
 
     /* This element should not be used in any of the PORT APIs.
      * It will be used by other modules or application to denote that none of
@@ -841,7 +846,7 @@ void PORT_GroupToggle(PORT_GROUP group, uint32_t mask);
     void PORT_GroupInputEnable(PORT_GROUP group, uint32_t mask)
 
   Summary:
-    Confgiures the selected IO pins of a group as input.
+    Configures the selected IO pins of a group as input.
 
   Description:
     This function configures the selected IO pins of a group as input. The pins
@@ -880,7 +885,7 @@ void PORT_GroupInputEnable(PORT_GROUP group, uint32_t mask);
     void PORT_GroupOutputEnable(PORT_GROUP group, uint32_t mask)
 
   Summary:
-    Confgiures the selected IO pins of a group as output.
+    Configures the selected IO pins of a group as output.
 
   Description:
     This function configures the selected IO pins of a group as output. The pins
